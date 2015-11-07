82d04a3842dbe9fb6c3e21cdd880006fdfbbf1f6
Anthony Liu <anthonyscottliu@gmail.com>
2015-11-07 01:58:52 -0500

    Takes an image input and redraws it with manipulated pixels.

--- js/reedr.js
+++ js/reedr.js
@@ -11,22 +11,99 @@ var Reedr = (function() {
   'use strict';
 
   /**********
-  * config */
+   * config */
+  var DISP_WID = 600;
 
   /*************
-  * constants */
+   * constants */
 
   /*********************
-  * working variables */
+   * working variables */
+  var canvas, ctx;
+  var dims, pixels;
 
   /******************
   * work functions */
   function initReedr() {
-    //swag
+    canvas = $s('#canvas');
+    canvas.style.width = DISP_WID+'px';
+    canvas.style.height = DISP_WID+'px';
+    ctx = canvas.getContext('2d');
+
+    dims = [0, 0], pixels = [];
+    $s('#image-sel').addEventListener('change', function(e) {
+      updatePixelData(e, displayImage);
+    });
+  }
+
+  //displays the currently loaded image
+  function displayImage() {
+    canvas.style.height = (DISP_WID*(dims[1]/dims[0]))+'px';
+    canvas.width = dims[0];
+    canvas.height = dims[1];
+    var currImageData = ctx.getImageData(0, 0, dims[0], dims[1]);
+		for (var y = 0; y < dims[1]; y++) { //for all intended rows
+			for (var x = 0; x < dims[0]; x++) { //and for each intended column
+				var idx = 4*(dims[0]*y + x); //idx of this pixel in the pixels array
+				currImageData.data[idx] = pixels[idx+1];
+				currImageData.data[idx+1] = pixels[idx];
+				for (var c = 2; c < 4; c++) { //and for all three colors, lol c++
+					currImageData.data[idx+c] = pixels[idx+c];
+				}
+			}
+		}
+		ctx.putImageData(currImageData, 0, 0);
+  }
+
+  //from http://www.syntaxxx.com/accessing-user-device-photos-
+  //     with-the-html5-camera-api/
+  //updates the pixels array with the selected image's data
+  function updatePixelData(e, callback) {
+    //bring selected photo in
+    var fileInput = e.target.files;
+    if (fileInput.length > 0) {
+      //get the file
+      var windowURL = window.URL || window.webkitURL;
+      var picURL = windowURL.createObjectURL(fileInput[0]);
+      getPixelsFromImage(picURL, function(data, width) {
+        dims = [width, data.length/(4*width)];
+        pixels = data;
+        windowURL.revokeObjectURL(picURL);
+
+        callback();
+      });
+    }
   }
 
   /********************
-  * helper functions */
+   * helper functions */
+  //given a url, provides a callback with the pixel data of the corresp image
+  function getPixelsFromImage(location, callback) { //returns array of px colors
+   	var timeStartedGettingPixels = new Date().getTime();
+   	var img = new Image(); //make a new image
+   	img.onload = function() { //when it is finished loading
+   		var canvas = document.createElement('canvas'); //make a canvas element
+   		canvas.width = img.width; //with this width
+   		canvas.height = img.height; //and this height (the same as the img)
+   		canvas.style.display = 'none'; //hide it from the user
+   		document.body.appendChild(canvas); //then add it to the document's body
+   		var ctx = canvas.getContext('2d'); //now get the context
+   		ctx.drawImage(img, 0, 0, img.width, img.height); //so that you can draw it
+   		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+   		document.body.removeChild(canvas); //all done, so get rid of it
+
+   		//...all so you can send the pixels, width, and the time taken to get
+       //them back through the callback
+   		var ret = [];
+   		for (var ai = 0; ai < imageData.data.length; ai++) {
+         ret.push(imageData.data[ai]); //annoying copy so the array can be edited
+   		}
+       callback(ret, img.width, new Date().getTime() - timeStartedGettingPixels);
+   	};
+
+   	img.src = location; //load the image
+  }
+
   function $s(id) { //for convenience
     if (id.charAt(0) !== '#') return false;
     return document.getElementById(id.substring(1));
