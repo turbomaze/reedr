7d8bf5f54c4b29522dae02d581c5b021d304d6e6
Anthony Liu <anthonyscottliu@gmail.com>
2015-11-08 07:50:35 -0500

    so soon ahhh

--- a.js
+++ a.js
@@ -12,8 +12,6 @@
 var Reedr = (function() {
   /**********
    * config */
-  var DISP_WID = 180;
-  var WORD_DISP_HT = 40;
 
   /*************
    * constants */
@@ -38,8 +36,6 @@ var Reedr = (function() {
   function initReedr() {
     //canvas stuff
     canvas = $s('#c');
-    canvas.style.width = DISP_WID+'px';
-    canvas.style.height = DISP_WID+'px';
     ctx = canvas.getContext('2d');
 
     //misc variable init
@@ -57,22 +53,12 @@ var Reedr = (function() {
         mapping = getMappingFromLabels(labelWords(getSmoothing(getSmoothing(colToBW(
           pixels, dw
         )))), dw);
-        boxes = getBoxes(mapping);
 
         //sort the boxes
-        boxes = sortBoxes(boxes);
+        boxes = sortBoxes(getBoxes(mapping));
 
         //get a nice gray representation of the image
         gray = colToGray(pixels, dw, true, 1.6);
-
-        //prep the canvas
-        canvas.width = DISP_WID;
-        canvas.height = WORD_DISP_HT;
-        canvas.style.width = 4*DISP_WID+'px';
-        canvas.style.height = 4*WORD_DISP_HT+'px';
-
-        //display the first word
-        displayWord(0);
 
         //automatically loop through them
         displayUntilEnd(60000/400);
@@ -82,20 +68,15 @@ var Reedr = (function() {
 
   function displayUntilEnd(delay) {
     if (wordIdx < boxes.length) {
-      displayWord(wordIdx);
+      drawWordPxArray(
+        getWordPicInBox(
+          gray[1], gray[0],
+          mapping, boxes[wordIdx]
+        ), boxes[wordIdx][2]
+      );
       wordIdx++;
       setTimeout(displayUntilEnd, delay, delay);
     }
-  }
-
-  //displays the word at the given index
-  function displayWord(idx) {
-    drawWordPxArray(
-      getWordPicInBox(
-        gray[1], gray[0],
-        mapping, boxes[idx]
-      ), boxes[idx][2]
-    );
   }
 
   //updates the pixels array with the selected image's data. From:
@@ -106,7 +87,7 @@ var Reedr = (function() {
     if (fileInput.length > 0) {
       //get the file
       var picURL = URL.createObjectURL(fileInput[0]);
-      getPixelsFromImage(picURL, 4*DISP_WID, function(data, width) {
+      getPixelsFromImage(picURL, 720, function(data, width) {
         //fix funky orientations
         if (width > data.length/(4*width)) {
           //rotate it
@@ -135,28 +116,6 @@ var Reedr = (function() {
         callback();
       });
     }
-  }
-
-  //displays the currently loaded image in BW and returns the BW data
-  function displayImageBW(bw) {
-    //fix the dimensions
-    canvas.style.display = 'block';
-    canvas.style.height = (DISP_WID*(dh/dw))+'px';
-    canvas.width = dw;
-    canvas.height = dh;
-
-    //display the bw image
-    var currImageData = ctx.getImageData(0, 0, dw, dh);
-    for (var y = 0; y < dh; y++) { //for all intended rows
-      for (var x = 0; x < dw; x++) { //and for each intended column
-        var idx = 4*(dw*y + x); //idx of this pixel in the pixels array
-        for (var c = 0; c < 3; c++) { //and for all three colors, lol c++
-          currImageData.data[idx+c] = fr(255*bw[idx/4]);
-        }
-        currImageData.data[idx+3] = 255;
-      }
-    }
-    ctx.putImageData(currImageData, 0, 0);
   }
 
   //blurs a black/white image using a gaussian kernal
@@ -258,7 +217,7 @@ var Reedr = (function() {
           var fl = fr(gray[idx]);
           pxs = pxs.u(fl, fl, fl, 255);
         } else {
-          pxs = pxs.u([255, 255, 255, 255]);
+          pxs = pxs.u(255, 255, 255, 255);
         }
       }
     }
@@ -269,10 +228,10 @@ var Reedr = (function() {
   //given an array of color info, render that to the canvas
   function drawWordPxArray(arr, width) {
     //display the bw image
-    var xOff = (canvas.width - width)/2;
-    var yOff = (canvas.height - arr.length/(4*width))/2;
+    var xOff = (180 - width)/2;
+    var yOff = (40 - arr.length/(4*width))/2;
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, 180, 40);
     var currImageData = ctx.getImageData(0, 0, width, arr.length/(4*width));
     for (var ai = 0; ai < arr.length; ai++) {
       currImageData.data[ai] = arr[ai];
@@ -308,15 +267,14 @@ var Reedr = (function() {
   }
 
   //converts an array of values to black and white, depending on a threshold
-  function grayToBW(data, width, thresh) {
-    return data.map(function(intensity) {
-      return intensity > thresh ? 1 : 0;
+  function grayToBW(data, width) {
+    return data[1].map(function(intensity) {
+      return intensity > 0.75*data[0] ? 1 : 0;
     });
   }
 
   //converts an RGBA image array into a grayscale image array
   function colToGray(data, width, wantAverage, contrast) {
-    contrast = contrast || 1;
     //convert to grayscale
     var gray = [], avgGray = 0;
     var ht = data.length/(4*width);
@@ -334,12 +292,10 @@ var Reedr = (function() {
   }
 
   //converts an RGBA image array into a binary array (black and white)
-  function colToBW(data, width, thresh) {
-    thresh = thresh || 0.75;
-
+  function colToBW(data, width) {
     //convert to grayscale
-    var grayAndAvg = colToGray(data, width, true);
-    return grayToBW(grayAndAvg[1], width, thresh*grayAndAvg[0]);
+    var grayAndAvg = colToGray(data, width, true, 1);
+    return grayToBW(grayAndAvg, width);
   }
 
   //given a url, provides a callback with the pixel data of the corresp image
@@ -416,9 +372,8 @@ var Reedr = (function() {
 
   //two dimensional gaussian distribution function with variance parameters
   function gaussDist(x, y) {
-    return (1/(2*Math.PI*1.6))*Math.exp(
-      -(x*x)/2 +
-      -(y*y)/(2*1.6*1.6)
+    return 0.1*Math.exp(
+      -(x*x)/2 - (y*y)/5.12
     );
   }
 
@@ -433,25 +388,14 @@ var Reedr = (function() {
           var pixel = key.split(',').map(function(comp) {
             return parseInt(comp);
           });
-          if (pixel[0] < minx) {
-            minx = pixel[0];
-          }
-
-          if (pixel[0] > maxx) {
-            maxx = pixel[0];
-          }
-
-          if (pixel[1] < miny) {
-            miny = pixel[1];
-          }
-
-          if (pixel[1] > maxy) {
-            maxy = pixel[1];
-          }
+          minx = mn(minx, pixel[0]);
+          miny = mn(miny, pixel[1]);
+          maxx = mx(maxx, pixel[0]);
+          maxy = mx(maxy, pixel[1]);
         });
         return [minx, miny, (maxx-minx), (maxy-miny), idx];
     }).q(function(box) {
-      return box[2] > 4 && box[2] > 4;
+      return box[2] > 4;
     });
   }
 
@@ -468,14 +412,13 @@ function sortBoxes(bxs) {
 
           bxs.r(function (box, index) {
               if (sameLine(last, box)) {
-                  line.p(box.u([index]));
+                  line.p(box.u(index));
               }
           });
 
           line = line.q(function (box){
               var distance = (box[0] + box[2]/2) - (last[0] + last[2]/2);
-              var maxDistance = box[2] + last[2];
-              return ((distance > 0) && (distance < maxDistance));
+              return ((distance > 0) && (distance < box[2] + last[2]));
           }).sort(function (b1, b2) {
             return (b1[0] + b1[2]/2) - (b2[0] + b2[2]/2);
           });
@@ -493,7 +436,7 @@ function sortBoxes(bxs) {
           next = bxs.reduce(function(a, b, idx) {
             if (a[0]+a[1]*slope < b[0]+b[1]*slope) {
               return a;
-            } else return b.u([idx]);
+            } else return b.u(idx);
           }, [1/0, 1/0, 1/0, 1/0, -1]);
       }
       sortedBoxes.p(bxs.splice(next[5], 1)[0]);
